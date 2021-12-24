dd77c5de5ba56d202061c9462b6bf6362158660e
Henry Swanson <henryswanson94@gmail.com>
2021-12-24 11:20:00 -0800

    Add very simple parse check
    Now instead of automatic success on parsing error, we at least check if
    we expected a parsing error too.
    
    This leaves a lot of tests failing, unfortunately. We'll get to them.

--- tests/integration.rs
+++ tests/integration.rs
@@ -6,6 +6,7 @@ use test_generator::test_resources;
 
 #[derive(Debug)]
 struct Expected {
+    compiled: bool,
     out: Vec<String>,
     runtime_err: Option<String>,
 }
@@ -17,51 +18,64 @@ fn test_bytecode_vm(filename: &str) {
         return;
     }
 
-    // TODO: get rid of this
-    macro_rules! succeed_on_err {
-        ($e:expr) => {
-            match $e {
-                Ok(x) => x,
-                Err(_) => return,
-            }
-        };
-    }
-
     let source = std::fs::read_to_string(filename).unwrap();
 
-    // Get expected output, and prep buffer for actual output
+    // Get expected and real outputs
     let expected = get_expected_output(&source);
-    let mut out_buf = Vec::<u8>::with_capacity(expected.out.len());
+    let actual = run_source_on_vm(&source);
 
-    let parser = Parser::new(&source);
-    // TODO augment tests to catch parsing errors
-    let tree = succeed_on_err!(parser.parse_all());
+    assert_eq!(expected.compiled, actual.compiled);
+    assert_eq!(expected.out, actual.out);
+    assert_eq!(expected.runtime_err, actual.runtime_err);
+}
 
+fn run_source_on_vm(source: &str) -> Expected {
+    // Set up the VM
+    let mut out_buf = vec![];
     let mut vm = VM::new_with_output(std::io::Cursor::new(&mut out_buf));
-    let mut compiler = Compiler::new(vm.borrow_string_table());
 
-    // TODO augment tests to catch compiler errors
-    let main_fn = succeed_on_err!(compiler.compile(&tree.statements));
+    // Set up the output struct
+    let mut result = Expected {
+        compiled: false,
+        out: vec![],
+        runtime_err: None,
+    };
+
+    // Parse and compile the source
+    let parser = Parser::new(&source);
+    let tree = match parser.parse_all() {
+        Ok(tree) => tree,
+        Err(_) => return result,
+    };
+
+    let mut compiler = Compiler::new(vm.borrow_string_table());
+    let main_fn = match compiler.compile(&tree.statements) {
+        Ok(f) => f,
+        Err(_) => return result,
+    };
+    result.compiled = true;
+
+    // Run the compiled function on the VM
     let vm_result = vm.interpret(main_fn);
 
-    // Check the expected and actual outputs
-    let out_lines: Vec<String> = String::from_utf8(out_buf)
+    // Set the output appropriately
+    result.out = String::from_utf8(out_buf)
         .unwrap()
         .lines()
         .map(|x| x.to_owned())
         .collect();
+    result.runtime_err = vm_result.err().map(|e| e.to_string());
 
-    assert_eq!(expected.out, out_lines);
-
-    // Check the runtime errors
-    assert_eq!(expected.runtime_err, vm_result.err().map(|e| e.to_string()));
+    result
 }
 
 fn get_expected_output(source: &str) -> Expected {
     let output_regex = Regex::new(r"// expect: (.*)$").unwrap();
     let runtime_err_regex = Regex::new(r"// expect runtime error: (.*)$").unwrap();
+    let compile_err_regex = Regex::new(r"// (\[line \d+\] )?(Error .*)").unwrap();
 
     let mut ex = Expected {
+        compiled: true,
         out: vec![],
         runtime_err: None,
     };
@@ -73,6 +87,9 @@ fn get_expected_output(source: &str) -> Expected {
         if let Some(m) = runtime_err_regex.captures(line) {
             ex.runtime_err
                 .replace(m.get(1).unwrap().as_str().to_owned());
+        }
+        if let Some(m) = compile_err_regex.captures(line) {
+            ex.compiled = false;
         }
     }
 
